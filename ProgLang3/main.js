1cbe403d353bbb0375b1a7f685d385f4690cdc9d
kiseme <33290981+kiseme@users.noreply.github.com>
2017-12-21 03:23:45 +0700

    Add files via upload

---
+++ ProgLang3/main.js
@@ -0,0 +1,12 @@
+﻿let x = new XMLHttpRequest();
+let Events = {};
+let Body = document.getElementsByTagName('body')[0];
+x.open("GET", "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.address=Boston&subcategories=2005%2C2007%2C2999&token=WKFJTUI4NXRQBOLGHL4C", true);
+x.onload = function () {
+	MegaObject = JSON.parse(x.response);
+	Events = MegaObject.events;
+	Events.forEach((event) => {
+		Body.innerText = Body.innerText + event.start.local + ' ' + event.name.text + '\n' + event.description.text + '\n\n';
+	})
+};
+x.send();
