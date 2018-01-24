ebe6cc98bac8477fd0c7b7e2bedeab1839ab3de5
kiseme <33290981+kiseme@users.noreply.github.com>
2018-01-25 01:21:25 +0700

    Update lab5.rs

--- ProgLang5/lab5.rs
+++ ProgLang5/lab5.rs
@@ -1,16 +1,5 @@
 use std::io;
-
-struct Point {
-    x: f64,
-    y: f64,
-}
-
-#[allow(dead_code)]
-struct Line {
-    p1: Point,
-    p2: Point,
-}
-
+use std::cmp;
 const N: i32 = 5;
 
 fn main() {
@@ -38,10 +27,31 @@ let mut Lines: Vec<Vec<f32>> = vec![];
   let B2: f32 = Lines[j][2] - Lines[j][0];
   let C1: f32 = Lines[0][0] * Lines[0][3] - Lines[0][2] * Lines[0][1];
   let C2: f32 = Lines[j][0] * Lines[j][3] - Lines[j][2] * Lines[j][1];
+  let mut X0: f32;
+  let mut X1: f32;
+  let mut Y0: f32;
+  let mut Y1: f32;
     if A1*B2-A2*B1 != 0.0 {
       println!("пересеклось");
       let X: f32 = -((C1*B2)-(C2*B1))/((A1*B2)-(A2*B1));
       let Y: f32 = -((A1*C2)-(A2*C1))/((A1*B2)-(A2*B1));
+      if Lines[j][0] < Lines[j][2]{
+        X0 = Lines[j][0];
+        X1 = Lines[j][2];
+      } else {
+        X1 = Lines[j][0];
+        X0 = Lines[j][2];
+      }
+      if Lines[j][1] < Lines[j][3]{
+        Y0 = Lines[j][1];
+        Y1 = Lines[j][3];
+      } else {
+        Y1 = Lines[j][1];
+        Y0 = Lines[j][3];
+      }
+      if X > X0 && X < X1 && Y > Y0 && Y < Y1{
+        println!("попало!");
+        }
     }
     println!("{:?}", Lines[j]); 
   }
